2f872925642df3a0b34a8dd88a4084c76c8b5223
rsravankumar1109 <rsravan@aapthitech.com>
2024-05-14 14:46:58 +0530

    changes
    changes

--- src/components/useDebounceEffect.ts
+++ src/components/useDebounceEffect.ts
@@ -3,11 +3,15 @@ import { useEffect, DependencyList } from 'react'
 export function useDebounceEffect(
   fn: () => void,
   waitTime: number,
-  deps?: DependencyList,
+  deps: DependencyList | undefined = undefined,
 ) {
   useEffect(() => {
     const t = setTimeout(() => {
-      fn.apply(undefined, deps)
+      if (deps) {
+        fn.apply(undefined, deps as [])
+      } else {
+        fn()
+      }
     }, waitTime)
 
     return () => {
