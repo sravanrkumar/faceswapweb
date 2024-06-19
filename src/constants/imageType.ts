94ad26ace4897aeaf80883153273200dfd61267b
rsravankumar1109 <rsravan@aapthitech.com>
2024-06-19 16:40:51 +0530

    changes

---
+++ src/constants/imageType.ts
@@ -0,0 +1,4 @@
+export const FILE_SIZE_LIMIT_MB = 25;
+
+export const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/gif", "video/mp4"];
+export const ALLOWED_VIDEO_TYPES = ["video/mp4"];
